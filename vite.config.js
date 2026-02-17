c1fef0e74a015e24fff2965ed9dc5984be6d6e06
Emerson Ferreira Rodrigues <104386868+Rfemerson@users.noreply.github.com>
2026-02-17 11:35:26 -0300

    Add route in vite.config.js for others pages.

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,8 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
+        watchlist: resolve(__dirname, "src/watchlist/index.html"),
+        movie: resolve(__dirname, "src/movie-details/index.html"),
       },
     },
   },
